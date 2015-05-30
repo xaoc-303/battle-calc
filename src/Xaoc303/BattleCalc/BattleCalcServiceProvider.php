dbeec04426aaa2417baea4cc5d7f4462b610dba4
Andrey Novikov <novikov.andrey.01@gmail.com>
2015-05-31 01:49:39 +0300

    init

---
+++ src/Xaoc303/BattleCalc/BattleCalcServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Xaoc303\BattleCalc;
+
+use Illuminate\Support\ServiceProvider;
+
+class BattleCalcServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('xaoc303/battle-calc');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
